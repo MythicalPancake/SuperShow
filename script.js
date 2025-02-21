cc544e4240c7706abfc939648d40735715f4d235
MythicalPancake <Pizzapizza0826@gmail.com>
2025-02-21 17:38:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -21,8 +21,7 @@ document.addEventListener("DOMContentLoaded", () => {
   let cardDatabase = [
     { name: "Fire Dragon", number: 5, competitor: "Alpha", entrance: "Main", image: "fire_dragon.jpg" },
     { name: "Water Serpent", number: 10, competitor: "Beta", entrance: "Side", image: "water_serpent.jpg" },
-    { name: "Earth Golem", number: 15, competitor: "Gamma", entrance: "Main", image: "earth_golem.jpg" },
-    { name: "Earth Go", competitor: "Hello", image: "earth_go.jpg" }
+    { name: "Earth Golem", number: 15, competitor: "Gamma", entrance: "Main", image: "earth_golem.jpg" }
   ];
 
   // Navigation: show one page at a time.
